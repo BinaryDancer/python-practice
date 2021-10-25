6b42adb5b7178ae9e13d6e0e06fa0168133f490b
BinaryDancer <pav.mamaew@yandex.ru>
2021-10-25 16:31:26 +0300

    Module 1: basic functions

---
+++ robust-modeling/module-1-BinaryDancer/README.md
@@ -0,0 +1,12 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=6016192&assignment_repo_type=AssignmentRepo)
+# MiniTorch Module 1
+
+<img src="https://minitorch.github.io/_images/match.png" width="100px">
+
+* Docs: https://minitorch.github.io/
+
+* Overview: https://minitorch.github.io/module1.html
+
+This assignment requires the following files from the previous assignments.
+
+        minitorch/operators.py minitorch/module.py tests/test_module.py tests/test_operators.py project/run_manual.py
---
+++ robust-modeling/module-1-BinaryDancer/minitorch/operators.py
@@ -0,0 +1,198 @@
+"""
+Collection of the core mathematical operators used throughout the code base.
+"""
+
+
+import math
+
+# ## Task 0.1
+
+# Implementation of a prelude of elementary functions.
+
+
+def mul(x, y):
+    return x * y
+
+
+def id(x):
+    return x
+
+
+def add(x, y):
+    return x + y
+
+
+def neg(x):
+    return -x
+
+
+def lt(x, y):
+    return 1.0 if x < y else 0.0
+
+
+def eq(x, y):
+    return 1.0 if x == y else 0.0
+
+
+def max(x, y):
+    return x if x > y else y
+
+
+def is_close(x, y):
+    return 1.0 if abs(x - y) < 1e-2 else 0.0
+
+
+def sigmoid(x):
+    r"""
+    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}`
+
+    (See `<https://en.wikipedia.org/wiki/Sigmoid_function>`_ .)
+
+    Calculate as
+
+    :math:`f(x) =  \frac{1.0}{(1.0 + e^{-x})}` if x >=0 else :math:`\frac{e^x}{(1.0 + e^{x})}`
+
+    for stability.
+
+    Args:
+        x (float): input
+
+    Returns:
+        float : sigmoid value
+    """
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x):
+    """
+    :math:`f(x) =` x if x is greater than 0, else 0
+
+    (See `<https://en.wikipedia.org/wiki/Rectifier_(neural_networks)>`_ .)
+
+    Args:
+        x (float): input
+
+    Returns:
+        float : relu value
+    """
+    return x if x > 0.0 else 0.0
+
+
+EPS = 1e-6
+
+
+def log(x):
+    ":math:`f(x) = log(x)`"
+    return math.log(x + EPS)
+
+
+def exp(x):
+    ":math:`f(x) = e^{x}`"
+    return math.exp(x)
+
+
+def log_back(x, d):
+    r"If :math:`f = log` as above, compute d :math:`d \times f'(x)`"
+    return d * 1.0 / (x + EPS)
+
+
+def inv(x):
+    ":math:`f(x) = 1/x`"
+    return 1.0 / x
+
+
+def inv_back(x, d):
+    r"If :math:`f(x) = 1/x` compute d :math:`d \times f'(x)`"
+    return d * (-1.0) / (x**2)
+
+
+def relu_back(x, d):
+    r"If :math:`f = relu` compute d :math:`d \times f'(x)`"
+    return d if x > 0 else 0.0
+
+
+# ## Task 0.3
+
+# Small library of elementary higher-order functions for practice.
+
+
+def map(fn):
+    """
+    Higher-order map.
+
+    .. image:: figs/Ops/maplist.png
+
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+
+    Args:
+        fn (one-arg function): Function from one value to one value.
+
+    Returns:
+        function : A function that takes a list, applies `fn` to each element, and returns a
+        new list
+    """
+    return lambda x: [fn(el) for el in x]
+
+
+def negList(ls):
+    "Use :func:`map` and :func:`neg` to negate each element in `ls`"
+    return map(neg)(ls)
+
+
+def zipWith(fn):
+    """
+    Higher-order zipwith (or map2).
+
+    .. image:: figs/Ops/ziplist.png
+
+    See `<https://en.wikipedia.org/wiki/Map_(higher-order_function)>`_
+
+    Args:
+        fn (two-arg function): combine two values
+
+    Returns:
+        function : takes two equally sized lists `ls1` and `ls2`, produce a new list by
+        applying fn(x, y) on each pair of elements.
+
+    """
+    return lambda x, y: [fn(elx, ely) for elx, ely in zip(x, y)]
+
+
+def addLists(ls1, ls2):
+    "Add the elements of `ls1` and `ls2` using :func:`zipWith` and :func:`add`"
+    return zipWith(add)(ls1, ls2)
+
+
+def reduce(fn, start):
+    r"""
+    Higher-order reduce.
+
+    .. image:: figs/Ops/reducelist.png
+
+
+    Args:
+        fn (two-arg function): combine two values
+        start (float): start value :math:`x_0`
+
+    Returns:
+        function : function that takes a list `ls` of elements
+        :math:`x_1 \ldots x_n` and computes the reduction :math:`fn(x_3, fn(x_2,
+        fn(x_1, x_0)))`
+    """
+    def func(ls):
+        y = start
+        for el in ls:
+            y = fn(el, y)
+        return y
+    return func
+
+
+def sum(ls):
+    "Sum up a list using :func:`reduce` and :func:`add`."
+    return reduce(add, 0)(ls)
+
+
+def prod(ls):
+    "Product of a list using :func:`reduce` and :func:`mul`."
+    return reduce(mul, 1)(ls)
