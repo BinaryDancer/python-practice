4b37a9cec958588568df3a1fb9ddd71347ccefb2
BinaryDancer <pav.mamaew@yandex.ru>
2021-10-31 16:35:47 +0300

    HW7 fixed exception

--- python-sc/hw7/seqjoin.py
+++ python-sc/hw7/seqjoin.py
@@ -1,5 +1,11 @@
+from itertools import chain
+
+
 def joinseq(*args):
-    my_args: list = [[next(seq), seq] for seq in [iter(el) for el in args]]
+    my_args = []
+    for seq in [iter(el) for el in args]:
+        new_it = chain.from_iterable([seq, ["abracadabra"]])
+        my_args.append([next(new_it), new_it])
 
     while my_args:
         idx = 0
@@ -8,8 +14,7 @@ def joinseq(*args):
             if v < min_v:
                 min_v = v
                 idx = i
-        try:
-            my_args[idx][0] = next(my_args[idx][1])
-        except Exception as e:
+        my_args[idx][0] = next(my_args[idx][1])
+        if my_args[idx][0] == "abracadabra":
             my_args.pop(idx)
         yield min_v
